a894c14d44377110f19ee3892ca23b5bd46ca1b6
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-12 10:42:23 +0900

    f

--- Test/CS_MSTest_Exercise/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Test/CS_MSTest_Exercise/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed8d284faaf0a2cd1848b4bf50b0a1a43aaaaae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+defa26e22c55e25593c420db45960001a3dd25e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
