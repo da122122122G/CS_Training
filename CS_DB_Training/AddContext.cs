0370e6b7c7bbf25e0535bbe935880aa4777204eb
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-12 13:49:18 +0900

    Add

---
+++ CS_DB_Training/AddContext.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+namespace CS_DB_Sample.Infrastructures;
+
+/// <summary>
+/// DbContext継承クラス
+/// WSL(Ubutu24.0.4)
+/// PostgreSQL17
+/// </summary>
+/// <author>Fullness,Inc.</author>
+/// <date>2025-11-21</date>
+/// <version>1.0.0</version>
+public class AppDbContext : DbContext
+{
+    /// <summary>
+    /// DbContextの構成処理をする
+    /// データベース接続情報の設定や、SQLログ出力の有効化する
+    /// </summary>
+    /// <param name="optionsBuilder">
+    /// DbContextの動作設定を構築するためのオプションビルダーオブジェクト
+    /// 接続先データベースやログ設定などを定義
+    /// </param>
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // 接続文字列（サーバー名、DB名、ユーザー名、パスワード）
+        string connectionString =
+            "Host=localhost;Database=cs_db_exercise;Username=appuser;Password=training;";
+        optionsBuilder
+        // PostgreSQLデータベースに接続する設定
+        // - connectionString：接続文字列（サーバー名、DB名、ユーザー名、パスワード）
+        .UseNpgsql(connectionString)
+        // 実行されたSQLをコンソールに表示する
+        // - SQL文が目に見えるので「どんなSQLが発行されているか」が分かる
+        // - デバッグや学習に非常に便利
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        // パラメーターの値もログに表示する
+        // - SQLに渡された値（例: "商品名 = '鉛筆'"）も確認できる
+        // - デバッグ時は便利だが、個人情報などを扱う本番環境では使わない
+        .EnableSensitiveDataLogging();
+    }
+}
